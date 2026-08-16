0ad3f9f96f5e9896392e82187aba3edcfd3259c1
felix <felix@pop-os.tail921051.ts.net>
2026-08-16 21:25:20 +0200

    Remove obsolete scripts

--- scripts/run-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "=== Arrancando OAuth2Server en modo DEV ==="
-
-export SPRING_PROFILES_ACTIVE=dev
-
-# Ejecutar la aplicación (sin redirección a /data)
-java -Dspring.profiles.active=dev -Dserver.address=0.0.0.0 -jar /app/app.jar
--- scripts/run-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "=== Arrancando OAuth2Server en modo PRODUCCIÓN ==="
-echo "JWT_SIGNING_KEY está definida: $(if [ -n \"$JWT_SIGNING_KEY\" ]; then echo \"SÍ\"; else echo \"NO\"; fi)"
-
-export SPRING_PROFILES_ACTIVE=prod
-
-exec java -jar /app/app.jar
